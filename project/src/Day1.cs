7a82eaee11b7ad18ed90be2efbc97122259f9c46
Nathaniel Curnick <nathaniel.curnick@gmail.com>
2023-12-17 16:09:48 +0000

    day 1

--- project/src/Day1.cs
+++ project/src/Day1.cs
@@ -1,9 +1,67 @@
-class Day1
+internal class Day1
 {
     public void Whole()
     {
+
         Console.WriteLine("Day 1");
+        string file = "day1.txt";
+        int calories = this.Part1(file);
+
+        Console.WriteLine(String.Format("Day 1 Part 1 {0}", calories.ToString()));
+
+        int topCalories = this.Part2(file);
+
+        Console.WriteLine(String.Format("Day 1 Part 1 {0}", topCalories.ToString()));
+
     }
 
+    internal int Part1(string file)
+    {
 
+        string[] lines = DataReader.ReadLines(file);
+
+        List<int> calories = this.FindCalories(lines);
+
+        return calories.Max();
+    }
+
+    internal int Part2(string file)
+    {
+        string[] lines = DataReader.ReadLines(file);
+
+        List<int> calories = this.FindCalories(lines);
+
+        calories.Sort((x, y) => y.CompareTo(x));
+
+        return calories[0] + calories[1] + calories[2];
+    }
+
+    private List<int> FindCalories(string[] lines)
+    {
+        List<int> calories = [];
+
+        List<int> tmp = [];
+        foreach (string line in lines)
+        {
+            if (line == "")
+            {
+                int calorie = tmp.Sum();
+                calories.Add(calorie);
+                tmp = [];
+            }
+            else
+            {
+                int calorie = int.Parse(line);
+                tmp.Add(calorie);
+            }
+        }
+
+        if (tmp.Count != 0)
+        {
+            int calorie = tmp.Sum();
+            calories.Add(calorie);
+        }
+
+        return calories;
+    }
 }
--- project/src/Program.cs
+++ project/src/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 using System.IO;
+using System.Reflection;
 
 
 class Program
@@ -13,9 +15,37 @@ class Program
 
         if (args[0] == "day1")
         {
-            Day1 day = new Day1();
+            Day1 day = new();
             day.Whole();
         }
     }
 }
 
+public class DataReader
+{
+    public static string[] ReadLines(string file)
+    {
+        string baseDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+
+        string parent = Directory.GetParent(baseDirectory).FullName;
+
+        for (int i = 0; i < 3; i++)
+        {
+            parent = Directory.GetParent(parent).FullName;
+        }
+        string path = Path.Combine(parent, "data", file);
+
+        try
+        {
+
+            string[] lines = File.ReadAllLines(path);
+            return lines;
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e);
+            throw new Exception(String.Format("Could not find file {0}", path));
+        }
+    }
+}
